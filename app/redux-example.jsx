f8bac9864e7155ffe178f721aa5de80abfcdadd8
Avinash Singh <avinash4507@gmail.com>
2017-03-27 01:10:11 +0530

    Added subscribe and dev tools

--- app/redux-example.jsx
+++
@@ -1,9 +0,0 @@
-var redux = require('redux');
-
-let reducer = (state = {name: 'Anonymous'}, action) => {
-    return state;
-}
-let store = redux.createStore(reducer);
-
-let currentState = store.getState();
-console.log('currentState', currentState);
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -6,10 +6,32 @@ let stateDefault = {
     showCompleted: false
 }
 let reducer = (state = stateDefault, action) => {
-    return state;
+    switch (action.type) {
+        case 'UPDATE_SEARCH_TEXT':
+            return {
+                ...state,
+                searchText: action.searchText
+            }    
+        default:
+            return state;
+    }
 }
 
-let store = redux.createStore(reducer);
+let store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+let unsubscribe = store.subscribe(() => {
+    let state = store.getState();
+}) 
 
 let currentState = store.getState();
-console.log('currentState', currentState);
+
+store.dispatch({
+    type: 'UPDATE_SEARCH_TEXT',
+    searchText: 'Avinash'
+});
+
+store.dispatch({
+    type: 'UPDATE_SEARCH_TEXT',
+    searchText: 'Singh'
+})
+
